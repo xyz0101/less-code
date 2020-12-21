da1b94d503f7ae051235f6bd5b33476f51523e9c
15115778766 <1394046585@qq.com>
2020-12-21 22:29:33 +0800

    redis保持连接

---
+++ common/src/main/java/com/jenkin/common/config/KeepRedisConnectBean.java
@@ -0,0 +1,44 @@
+package com.jenkin.common.config;
+
+import com.jenkin.common.utils.Redis;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.*;
+
+/**
+ * @author jenkin
+ * @className KeepRedisConnectUtils
+ * @description TODO
+ * @date 2020/12/21 17:53
+ */
+@Component
+@Slf4j
+public class KeepRedisConnectBean {
+
+    @Autowired
+    Redis redis;
+
+    private static final ScheduledThreadPoolExecutor SINGLE_SCHEDULED_EXECUTOR =  new ScheduledThreadPoolExecutor(1, new ThreadFactory() {
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            thread.setName("jenkin schedule keep redis connect");
+            return thread;
+        }
+    });
+
+    public  void run(){
+        SINGLE_SCHEDULED_EXECUTOR.schedule(()->{
+            log.info("keep redis connect");
+            redis.set("SINGLE_SCHEDULED_EXECUTOR","jenkin schedule keep redis connect");
+        },5, TimeUnit.MINUTES);
+    }
+    @PostConstruct
+    public void postConstruct(){
+        log.info(" 系统初始化 后台执行 keep redis connect");
+        this.run();
+    }
+}
